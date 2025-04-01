b032a56f0c02750631c12f3362e36e74f60245c7
xsorren <lauta.sorren@gmail.com>
2025-04-01 11:57:56 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,8 @@
 		// $('#preloader').delay(350).fadeOut('slow');
 		// $('body').delay(350).css({ 'overflow': 'visible' });
 		// -------------------- Site Preloader
-		$('#ctn-preloader').fadeOut(); // will first fade out the loading animation
-		$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+		// $('#ctn-preloader').fadeOut(); // will first fade out the loading animation
+		// $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
 		$('body').delay(350).css({ 'overflow': 'visible' });
 	});
 
